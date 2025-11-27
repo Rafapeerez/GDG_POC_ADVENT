4ab88251359bc715eaae114d8be7e69619dab75c
rafapeerez <rapeop1c@gmail.com>
2025-11-27 23:01:34 +0100

    feat: java solution and working on test

---
+++ challenges/week1/java/Solution.java
@@ -0,0 +1,16 @@
+package challenges.week1.java;
+
+public class Solution {
+    /**
+     * Cuenta el número de secciones vulnerables basado en los niveles de estabilidad
+     * y un umbral de seguridad.
+     *
+     * @param stabilities Array de enteros que representa los niveles de estabilidad
+     * @param securityThreshold Umbral de seguridad para determinar vulnerabilidad
+     * @return Número de secciones vulnerables encontradas
+     */
+    public static int countVulnerableSections(int[] stabilities, int securityThreshold) {
+        // TODO: Implementar la lógica para contar secciones vulnerables
+        return 0;
+    }
+}
